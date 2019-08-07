f1c020d0a8c590450a61066d5977d3eb1527d0e9
Michael728 <649168982@qq.com>
2019-08-07 23:21:21 +0800

    update log4j2

---
+++ spring-boot-logs/src/main/java/com/michael/springbootlogs/SpringBootLogsApplication.java
@@ -0,0 +1,13 @@
+package com.michael.springbootlogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLogsApplication.class, args);
+    }
+
+}
