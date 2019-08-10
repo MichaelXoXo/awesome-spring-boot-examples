9d331cb6764b3005d9e7fde70a85f03950dd3134
Michael728 <649168982@qq.com>
2019-08-10 17:31:02 +0800

    update logback

--- spring-boot-logback/src/main/java/com/michael/springbootlogback/controller/Demo.java
+++ spring-boot-logback/src/main/java/com/michael/springbootlogback/controller/Demo.java
@@ -1,5 +1,6 @@
 package com.michael.springbootlogback.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,40 +10,48 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Michael
  * @create 2019-08-09 20:53
  */
+@Slf4j
 @RestController
 public class Demo {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+//    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/debu")
     public String debug() {
-        logger.debug("logger: debug");
+        log.debug("log: debug");
         return "debug";
     }
 
     @GetMapping("/info")
     public String info() {
-        logger.info("logger: info");
+        log.info("log: info");
         return "info";
     }
 
     @GetMapping("/warn")
     public String warn() {
-        logger.warn("logger: warn");
+        log.warn("log: warn");
         return "warn";
     }
 
     @GetMapping("/error")
     public String error() {
-        logger.error("logger: error");
+        log.error("log: error");
         return "error";
     }
 
     @GetMapping("/all")
     public String all() {
-        logger.debug("logger: debug");
-        logger.info("logger: info");
-        logger.warn("logger: warn"+" 附件个信息");
-        logger.error("logger: error");
+        try {
+            log.debug("log: debug");
+            log.info("log: info");
+            log.warn("log: warn" + " 附件个信息");
+            System.out.println(1 / 0);
+        } catch (Exception e) {
+            log.error("error 打印方式比较，采用 ,e：", e);
+            log.error("error 打印方式比较，采用 +e : " + e);
+            log.error("error 打印方式比较，采用 e 括号 :{} " , e);
+            throw e;
+        }
         return "all";
     }
 }
