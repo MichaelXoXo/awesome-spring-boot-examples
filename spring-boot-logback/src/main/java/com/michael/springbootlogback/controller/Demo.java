4539190ba2306d4a7d4b9cc261a5d1dbd40eaaf6
Michael728 <649168982@qq.com>
2019-08-10 12:37:19 +0800

    update logback

---
+++ spring-boot-logback/src/main/java/com/michael/springbootlogback/controller/Demo.java
@@ -0,0 +1,48 @@
+package com.michael.springbootlogback.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Michael
+ * @create 2019-08-09 20:53
+ */
+@RestController
+public class Demo {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/debu")
+    public String debug() {
+        logger.debug("logger: debug");
+        return "debug";
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        logger.info("logger: info");
+        return "info";
+    }
+
+    @GetMapping("/warn")
+    public String warn() {
+        logger.warn("logger: warn");
+        return "warn";
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        logger.error("logger: error");
+        return "error";
+    }
+
+    @GetMapping("/all")
+    public String all() {
+        logger.debug("logger: debug");
+        logger.info("logger: info");
+        logger.warn("logger: warn"+" 附件个信息");
+        logger.error("logger: error");
+        return "all";
+    }
+}
